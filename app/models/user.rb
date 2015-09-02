56fa54a793ec0cfa2f4b60797b16a4be07b1c53b
Christian Reinhard <creinhard@uos.de>
2015-09-02 15:57:53 +0200

    fixed log in formular

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,12 +21,12 @@ class User < ActiveRecord::Base
       end
     end
 
-  #def login=(login)
-  #  @login = login
-  #end
-  #
-  #def login
-  #  @login || self.username || self.email
-  #end
+  def login=(login)
+    @login = login
+  end
+  
+  def login
+    @login || self.username || self.email
+  end
 
 end
