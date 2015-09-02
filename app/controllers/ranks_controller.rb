8bbf8cf64570a77117160391ddcddce029f35f04
naffing <naffing@cip64.informatik.uni-osnabrueck.de>
2015-09-02 10:55:22 +0200

    Changes UserEmail to UserID

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -69,6 +69,6 @@ class RanksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def rank_params
-      params.require(:rank).permit(:email, :score)
+      params.require(:rank).permit(:user_id, :score)
     end
 end
