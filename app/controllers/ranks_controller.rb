f368bd0e177493ea981dc8192ca62d768fdb35b4
naffing <naffing@cip64.informatik.uni-osnabrueck.de>
2015-09-03 11:38:55 +0200

    Replace Tabs with Spaces

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -20,52 +20,50 @@ class RanksController < ApplicationController
   # GET /ranks/1/edit
   def edit
   end
+  
+  def self.rankList(center, offset)
+    allRanks = Array.new
+    currentRank = nilUser.all.each do |u|
+      currentRank = Rank.find_by(user_id: u.id)
 
-	def self.rankList(center, offset)
-		allRanks = Array.new
+      if not currentRank
+        currentRank = Rank.new
+        currentRank.user_id = u.id
+        currentRank.score = 0
+        Rank.create(:user_id => u.id, :score => 0)
+      end
+      allRanks << currentRank
+    end
+    allRanks = allRanks.sort_by{|rank| rank.score}.reverse
 
-		currentRank = nil
-		User.all.each do |u|
-			currentRank = Rank.find_by(user_id: u.id)
-
-			if not currentRank
-				currentRank = Rank.new
-				currentRank.user_id = u.id
-				currentRank.score = 0
-				Rank.create(:user_id => u.id, :score => 0)
-			end
-			allRanks << currentRank
-		end
-		allRanks = allRanks.sort_by{|rank| rank.score}.reverse
-
-		if(center < 0)
-			center = 0
-		end
-		
-		if(offset < 0)
-			offset = 0
+    if(center < 0)
+      center = 0
     end
 
-		lower = center - offset
-		if(lower < 0)
-			lower = 0
+    if(offset < 0)
+      offset = 0
     end
-		upper = center + offset
+
+    lower = center - offset
+    if(lower < 0)
+      lower = 0
+    end
+    upper = center + offset
     
-		if(upper >= allRanks.length)
-			upper = allRanks.length - 1
-		end
+    if(upper >= allRanks.length)
+      upper = allRanks.length - 1
+    end
 
-		rankedNames = Hash.new("Default")
+    rankedNames = Hash.new("Default")
 
-		counter = lower
-		allRanks[lower..upper].each do |a|
-			counter = counter + 1
-			rankedNames[a] = counter
-    	end
+    counter = lower
+    allRanks[lower..upper].each do |a|
+      counter = counter + 1
+      rankedNames[a] = counter
+    end
 
-		return rankedNames
-	end
+    return rankedNames
+  end
 
   # POST /ranks
   # POST /ranks.json
