f34cf5aaab68815b58d42486f1aa2021149940c0
msteinkamp <msteinkamp@cip67.informatik.uni-osnabrueck.de>
2015-09-03 13:41:48 +0200

    Fixed Association

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,5 +1,10 @@
 class Ship < ActiveRecord::Base
 	has_many :ships_stations
-	has_many :stationtypes, through => :ships_station
-	
+	has_many :stationtypes, :through => :ships_station
+	around_create :create_stations
+
+	private
+	def create_stations
+
+	end
 end 
