9ddd51253a71512f10b80a2f64819af361a89bd1
naffing <naffing@cip64.informatik.uni-osnabrueck.de>
2015-09-07 12:48:01 +0200

    Improve TimeFormat

--- lib/TimeFormatter.rb
+++ lib/TimeFormatter.rb
@@ -4,5 +4,6 @@ def format_count_time(secs)
 	min = (secs / 60) % 60
 	sec = secs % 60
 
-	return day.to_i.to_s + ":" + hour.to_i.to_s + ":" + min.to_i.to_s + ":" + sec.to_i.to_s
+	return (day < 10 ? "0" : "") + day.to_i.to_s + ":" + (hour < 10 ? "0" : "") + hour.to_i.to_s + ":" + 
+	(min < 10 ? "0" : "") + min.to_i.to_s + ":" + (sec < 10 ? "0" : "") + sec.to_i.to_s
 end
