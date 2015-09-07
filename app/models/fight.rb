24cecb5ed364bd6dcd95740aa6bac324b6e1aa4d
andschroeder <andschroeder@cip72.informatik.uni-osnabrueck.de>
2015-09-07 13:13:53 +0200

    Edit Model.rb

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -10,35 +10,48 @@ class Fight#< ActiveRecord::Base
      @spy_level_attacker = 2
      @spy_level_defender = 1
      @spy_event = true
-     @ships = {s1: {name: "ship1", damage: 5, hp: 15}, s2: {name: "ship2", damage: 4, hp: 20}}
- 
+     
+     @ships = {s1: {name: "ship1", damage: 5, hp: 15, number: 4}, s2: {name: "ship2", damage: 4, hp: 20, number: 5}}
   end
 
   def time_to_fight
 #    time=self.time.to_i - Time.now.to_i 
     return 2 
   end
-
+  
+  # Methode to start the report-creating with the names of the attacker 
+  # and defender
   def report_start
    return "Kampfbericht: \n \n Angreifer: #{self.attacker.username} Verteidiger: #{self.defender.username} \n \n "
   end
 
-  def threshold
+  # Methode to calculate the threshold of for succesfull spying
+  def threshold_spy
     puts "Ergebnis:"
     puts (@spy_level_defender - @spy_level_attacker)*0.05+0.5  
     return (@spy_level_defender - @spy_level_attacker)*0.05+0.5  
   end
 
-  def random
+  # Calculate the threshold for a succesfull emp-event
+  def threshold_emp
+      return (1-(1/(1+0.5*@number_of_emp_ships)))
+  end
+
+  # Random float from 0 to 1
+  def random_to_one
     return rand()
   end 
-
-
+  
+  # Random float from 0 to x
+  def random_to_x(max)
+    return rand(max)
+  end
+  # Methode for the spying-event
   def spy_phase
     puts("Anzahl der Spionage Drohnen:" + @number_of_spy_probes.to_s)
     if @number_of_spy_probes.to_f > 0
-      puts tmp=random
-      if threshold > tmp
+      puts tmp_random=random_to_one
+      if threshold_spy > tmp_random
         #Spyprobes leave battle!!
         puts "Spionage geglückt!"
         return @spy_event=true
@@ -49,6 +62,7 @@ class Fight#< ActiveRecord::Base
     end
   end
   
+  # Adds the result of the spying to the report
   def spy_report
     tmp="  Spionagebericht:  \n \n "
     if (@spy_event)
@@ -57,11 +71,12 @@ class Fight#< ActiveRecord::Base
       tmp2= " #{tmp} Misserfolg! Ihre Dronen wurden zerstört. \n \n"
     end
   end
-
+  
+  # Trying to destroy shields
   def emp_phase
     if @number_of_emp_ships > 0
-      tmp = (1-(1/(1+0.5*@number_of_emp_ships)))
-      randomtmp = random
+      tmp = threshold_emp
+      randomtmp = random_to_one
       if tmp > randomtmp
         puts "Schilde zerstört!!! #{tmp} #{randomtmp}"
         return true
@@ -72,7 +87,28 @@ class Fight#< ActiveRecord::Base
     
   end
 
+  # Adds the result from the emp-event to the report
+  def emp_report
+    
+  end
 
+
+  def fight
+    attackers_turn =true
+    ship_ary =[]
+    ships.each do |key|
+      print key
+        key.each do |name, damage, hp, number|
+        puts " #{name} #{damage} #{hp} #{number}"
+      
+        end    
+      end
+    end
+
+  # Hilfsmethode zum testen, WIEDER LÖSCHEN!!!
+  def ships
+    return @ships
+  end
  
 
 
@@ -82,6 +118,5 @@ f=Fight.new
 #f.number_of_spy_probes = 1
 f.emp_phase
 f.spy_phase
-
-
+f.fight
 end
