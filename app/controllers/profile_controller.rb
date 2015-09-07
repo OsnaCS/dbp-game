b1bc5441751be6d83eaede4ac991fbc1e98e4eaf
Christian Reinhard <creinhard@uos.de>
2015-09-07 13:41:45 +0200

    improved profile linking

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -9,10 +9,4 @@ class ProfileController < ApplicationController
   	@user = User.find_by_username(params[:user])
   end
 
-  def upload
-    uploaded_io = params[:person][:picture]
-	File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-	  file.write(uploaded_io.read)
-	end
-  end
 end
