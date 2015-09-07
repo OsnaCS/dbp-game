e2cf5bb259a0352729f411305f6e27f18da1dfc3
naffing <naffing@cip64.informatik.uni-osnabrueck.de>
2015-09-07 12:54:57 +0200

    Generate Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907083521) do
+ActiveRecord::Schema.define(version: 20150907105417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,13 @@ ActiveRecord::Schema.define(version: 20150907083521) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "resources", force: :cascade do |t|
+    t.text     "name"
+    t.integer  "production"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "science_instances", force: :cascade do |t|
     t.integer  "science_id"
     t.integer  "user_id"
@@ -120,10 +127,11 @@ ActiveRecord::Schema.define(version: 20150907083521) do
     t.float    "factor"
     t.integer  "duration"
     t.string   "condition"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
     t.string   "name"
     t.integer  "tier"
+    t.integer  "science_condition_id"
   end
 
   create_table "ship_groups", force: :cascade do |t|
@@ -142,6 +150,9 @@ ActiveRecord::Schema.define(version: 20150907083521) do
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "metal"
+    t.integer  "cristal"
+    t.integer  "fuel"
   end
 
   create_table "ships_stations", force: :cascade do |t|
@@ -166,6 +177,7 @@ ActiveRecord::Schema.define(version: 20150907083521) do
     t.text     "description"
     t.integer  "condition"
     t.integer  "tier"
+    t.string   "icons"
   end
 
   create_table "teaparties", force: :cascade do |t|
@@ -190,6 +202,7 @@ ActiveRecord::Schema.define(version: 20150907083521) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.integer  "right_level"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
