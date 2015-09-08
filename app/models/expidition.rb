3740c1b471cb5429a11536f14eb3128b1e454f89
Patrick Pfeiffer <ppfeiffer@uos.de>
2015-09-08 10:52:15 +0200

    Unithandling during expedition

--- app/models/expidition.rb
+++ app/models/expidition.rb
@@ -1,7 +1,8 @@
 class Expidition < ActiveRecord::Base
 
    belongs_to :fighting_fleet
-   @storeroom
+   @exp_storeroom
+   @fleet_storeroom
    @explore_time
    @value
 
@@ -12,6 +13,17 @@ class Expidition < ActiveRecord::Base
    #end
 
    def explore
+      @value = 0
+      @fleet_storeroom = 0
+      @exp_storeroom = 0
+      fighting_fleet.ship_group.each do |g|
+        @value += g.unit.total_cost * g.number
+        @fleet_storeroom += g.unit.cargo * g.number
+        if(g.unit.name == "Expeditionsschiff")
+           #TODO Unit Verknuepfung ueberpruefen
+           @exp_storeroom += g.unit.cargo * g.number
+        end
+      end
       event = rand(100)
       happen = 1.0-(1.0/(1.0 + @explore_time))
       happen = happen * 100
@@ -42,15 +54,37 @@ class Expidition < ActiveRecord::Base
    end
 
    def destruction
-      puts "destroyed"
+      fighting_fleet.ship_group.each do |g|
+         g.number = 0
+      end
    end
 
    def fight
       random = rand(70..130)
       limit = random * @value/100.0
-      puts "Gegnerstärke: #{limit/@value}"
+      kreuzer_amount = 0
+      jaeger_amount = 0
+      fregatte_amount = 0
+      schild_amount = 0
+      until(limit<enemy_strength)
+         ship_got = rand(100)
+         case ship_got
+         when 0..10
+            enemy_strength += 100
+            kreuzer_amount += 1
+         when 10..30
+            enemy_strength += 40
+            fregatte_amount += 1
+         when 30..60
+            enemy_strength += 8
+            schild_amount += 1
+         when 60..100
+            enemy_strength += 4
+            jaeger_amount += 1
+         end
+      end
+         #TODO gegnerische Flotte erstellen basierend auf den amounts
    end
-
    def ressource
       first = rand(100)
       second = rand(100)
@@ -68,33 +102,64 @@ class Expidition < ActiveRecord::Base
          fuel = 100 - first
       end
       relative_amount = rand(30..200)
-      absolute_amount = @storeroom * relative_amount/10000.0
-      metal_got = absolute_amount * metal
-      crystal_got = absolute_amount * crystal
-      fuel_got = absolute_amount * fuel
-
-      puts "#{metal_got}, #{crystal_got}, #{fuel_got}"
+      absolute_amount = @exp_storeroom * relative_amount/10000.0
+      final_amount = min (absolute_amount, @fleet_storeroom)
+      metal_got = final_amount * metal
+      crystal_got = final_amount * crystal
+      fuel_got = final_amount * fuel
+      #TODO Nachrichten fuer die Ereignisse ausgeben
+      #TODO Ressourcen adden
       
    end
 
    def salvage
       threshold = @value/10.0
       gain = 0.0
+      kreuzer_amount = 0
+      jaeger_amount = 0
+      fregatte_amount = 0
+      schild_amount = 0
       until(threshold<gain)
          ship_got = rand(100)
          case ship_got
          when 0..10
             gain += 100
-            puts "Kreuzer"
+            kreuzer_amount += 1
          when 10..30
             gain += 40
-            puts "Fregatte"
+            fregatte_amount += 1
          when 30..60
             gain += 8
-            puts "mobiler Schild"
+            schild_amount += 1
          when 60..100
             gain += 4
-            puts "Jäger"
+            jaeger_amount += 1
+         end
+      end
+      #TODO Nachricht erstellen
+      fighting_fleet.ship_group.each do |g|
+         case g.unit.name
+         when "Kreuzer"
+            g.number += kreuzer_amount
+            kreuzer_amount = 0
+         when "Fregatte"
+            g.number += fregatte_amount
+         when "mobiler Schild"
+            g.number += schild_amount
+         when "Jäger"
+            g.number += jaeger_amount
+         end
+         if(kreuzer_amount != 0)
+            #TODO Kreuzer ship_group erstellen und amount adden
+         end
+         if(fregatte_amount != 0) 
+            #TODO fregatte ship_group erstellen und amount adden
+         end
+         if(schild_amount != 0)
+            #TODO schild ship_group erstellen und amount adden
+         end
+         if(jaeger_amount != 0)
+            #TODO jaeger ship_group erstellen und amount adden
          end
       end
    end
