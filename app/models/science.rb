e42886acdebb1e88fa23f47cab7682447ce99711
naffing <naffing@cip64.informatik.uni-osnabrueck.de>
2015-09-09 11:38:17 +0200

    Add ScienceInterface Remove get_sciences method

--- app/models/science.rb
+++ app/models/science.rb
@@ -5,24 +5,18 @@ class Science < ActiveRecord::Base
   has_many :users, :through => :science_instances
   validates_presence_of :cost1, :cost2, :cost3, :factor, :duration, :name, :tier, :science_condition_id, :icon
 
-  def self.get_sciences(user)
-    scienceHash = Hash.new(0)
-    user.science_instances.each do |s|
-      scienceHash[s.science] = s.level
+  # return -1 if user nil // -2 if id wrong
+  def self.get_science_level(user, id)
+    if(user.nil?)
+      return -1
+    end
+    instance = ScienceInstance.find_by(:user_id => user.id, :science_id => id)
+
+    if(instance.nil?)
+      return -2
     end
 
-    instance = nil
-    Science.all.each do |s|
-      if not(scienceHash.has_key? s)
-        instance = ScienceInstance.new
-        instance.science_id = s.id
-        instance.user_id = user.id
-        instance.level = 0
-        ScienceInstance.create(:science_id => s.id, :user_id => user.id, :level => 0)
-        scienceHash[s] = instance.level
-      end
-    end
-    return scienceHash
+    return instance.level
   end
 
   def self.update_time(instance, format)
