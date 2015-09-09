5eb33418ccfd959451a6469352fbaa63938147ff
pkechlo <pkechlo@cip65.informatik.uni-osnabrueck.de>
2015-09-09 09:45:32 +0200

    Add migration for rename column in stations; delete migrations for add and remove column in stations; Add migrations for column tier and icon in stations; Edit stations index.html.erb

--- db/migrate/20150908140414_remove_cost_m_ineral_from_stations.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCostMIneralFromStations < ActiveRecord::Migration
-  def change
-  	remove_column :stations, :costMIneral
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908140523) do
+ActiveRecord::Schema.define(version: 20150909074153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,12 +71,15 @@ ActiveRecord::Schema.define(version: 20150908140523) do
 
   create_table "stations", force: :cascade do |t|
     t.text     "name"
+    t.integer  "costMineral"
     t.integer  "costCristal"
     t.integer  "costFuel"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "initial_level"
-    t.integer  "costMineral"
+    t.integer  "tier"
+    t.string   "icon"
+    t.integer  "condition"
   end
 
   create_table "users", force: :cascade do |t|
