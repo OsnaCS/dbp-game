a71c755d5a28dc23156912a2682a3df56209bc09
Christian Reinhard <creinhard@uos.de>
2015-09-09 13:25:37 +0200

    updated db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150908122237) do
-=======
 ActiveRecord::Schema.define(version: 20150909074153) do
->>>>>>> a099e442ec7a0e40b7155371163b40e4edcca181
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,7 +77,9 @@ ActiveRecord::Schema.define(version: 20150909074153) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "initial_level"
-<<<<<<< HEAD
+    t.integer  "tier"
+    t.string   "icon"
+    t.integer  "condition"
   end
 
   create_table "user_icons", force: :cascade do |t|
@@ -92,11 +90,6 @@ ActiveRecord::Schema.define(version: 20150909074153) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-=======
-    t.integer  "tier"
-    t.string   "icon"
-    t.integer  "condition"
->>>>>>> a099e442ec7a0e40b7155371163b40e4edcca181
   end
 
   create_table "users", force: :cascade do |t|
@@ -113,6 +106,7 @@ ActiveRecord::Schema.define(version: 20150909074153) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.integer  "right_level",            default: 0,  null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
