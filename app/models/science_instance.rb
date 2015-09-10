cf38dc865d716038e2e2614dc51d54be4c64de3e
Alexander Altemoeller <aaltemoeller@uni-osnabrueck.de>
2015-09-10 14:55:31 +0200

    Refactor Code: Move Functions from Controller to Model

--- app/models/science_instance.rb
+++ app/models/science_instance.rb
@@ -32,7 +32,22 @@ class ScienceInstance < ActiveRecord::Base
     return back.html_safe
   end
 
-  def get_conditions()
+  def level_cap_reached
+    lvl = self.level
+    cap =science.level_cap
+
+    if cap < 0
+      return false
+    end
+
+    return lvl >= cap
+  end
+
+  def get_conditions
+    if(self.level_cap_reached)
+      return "Level Max<br>".html_safe
+    end
+
   	info = self.science.condition
   	conds = info.split(",")
 
@@ -41,16 +56,16 @@ class ScienceInstance < ActiveRecord::Base
       back = back + "Aktuell wird geforscht...<br>"
   	end
 
-    if(conds.length != 0)
-  	 back = back + "Voraussetzung: <br>"
-    end
   	conds.each do |cond|
   		c_info = cond.split(":")
   		typ = c_info[0]
   		id_geb = c_info[1].to_i
   		lvl = c_info[2]
-  		name = Science.find_by(:science_condition_id => id_geb).name
-  		back = back+"- Forschung: "+name+" "+lvl.to_s+"<br>"
+  		science = Science.find_by(:science_condition_id => id_geb)
+
+      if not(user.has_min_science_level(science, lvl))
+        back = back+"- Forschung: "+science.name+" "+lvl.to_s+"<br>"
+      end
   	end
     
     leftMetal = science.get_metal_cost(self.level) - user.get_metal
@@ -68,6 +83,45 @@ class ScienceInstance < ActiveRecord::Base
     if(leftFuel > 0)
       back = back + "- Fehlender Treibstoff: " + leftFuel.to_s + "<br>"
     end
+
+    if(back.length != 0)
+      back = "Voraussetzung: <br>"+back
+    end
+
   	return back.html_safe
+  end
+
+  def update_time(format)
+    science = Science.find_by(id: self.science_id)
+    durationInSeconds = science.get_duration(self.level)
+
+    if(self.start_time)
+      timeSinceResearch = self.get_time_since_research
+      restTime = durationInSeconds - timeSinceResearch
+
+      if(restTime <= 0)
+        self.level = self.level + 1
+        self.start_time = nil
+        self.save
+
+        if not(format)
+          return durationInSeconds;
+        else
+          return format_count_time(durationInSeconds)
+        end
+      else
+        if not(format)
+          return restTime;
+        else
+          return format_count_time(restTime)
+        end
+      end
+    else
+      if not(format)
+        return durationInSeconds;
+      else
+        return format_count_time(durationInSeconds)
+      end
+    end
   end
 end
