c39f32ed6ed3cc5d8f478ba7e4cbed03513525c0
Alexander Altemoeller <aaltemoeller@uni-osnabrueck.de>
2015-09-10 12:56:21 +0200

    Generate Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909140635) do
+ActiveRecord::Schema.define(version: 20150910105608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,9 +74,10 @@ ActiveRecord::Schema.define(version: 20150909140635) do
     t.integer  "science_id"
     t.integer  "user_id"
     t.integer  "level"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.time     "start_time"
+    t.integer  "research_ship"
   end
 
   create_table "sciences", force: :cascade do |t|
