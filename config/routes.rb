a61279b5d6849fdd36ee13dbe58df57efdbb4fb2
atessmer <atessmer@cip69.informatik.uni-osnabrueck.de>
2015-09-10 12:16:37 +0200

    Reorganized routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :facilities
+
 
   resources :science_instances do
     member do
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :messages
   resources :stations
   resources :sciences
+  resources :facilities
   resources :ranks
   resources :user_icons
   resources :ships
