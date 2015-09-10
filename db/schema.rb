1422a5d9f150c568dc58cf92cf801d95a3d47f80
msteinkamp <msteinkamp@cip67.informatik.uni-osnabrueck.de>
2015-09-10 15:03:19 +0200

    Change datatype of name to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910092102) do
+ActiveRecord::Schema.define(version: 20150910125746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20150910092102) do
   end
 
   create_table "ships", force: :cascade do |t|
-    t.text     "name"
+    t.string   "name"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "metal"
