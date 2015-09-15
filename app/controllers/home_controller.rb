c26d35411f45ba43956ce4542da5f22a065ac052
msteinkamp <msteinkamp@cip67.informatik.uni-osnabrueck.de>
2015-09-15 12:00:07 +0200

    nzeigen von Rohstoffen zu int ändern.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,9 +7,9 @@ class HomeController < ApplicationController
   		@notification_count = Notification.all.where(user_id: current_user.id).count
   		@ship.update_resources
 
-  		@metal_count = @ship.metal
-  		@crystal_count = @ship.cristal
-  		@fuel_count = @ship.fuel
+  		@metal_count = @ship.metal.to_i
+  		@crystal_count = @ship.cristal.to_i
+  		@fuel_count = @ship.fuel.to_i
       @metal_storage_cap = @ship.max_storage(2008).to_i
       @crystal_storage_cap = @ship.max_storage(2009).to_i
       @fuel_storage_cap = @ship.max_storage(2010).to_i
